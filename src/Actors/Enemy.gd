64fe07327809cb8938cef8828ab1b6a2b1dea0c5
Mark Daly <118356771@umail.ucc.ie>
2022-02-14 03:40:24 +0000

    Code enemy movement and animation

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -1,2 +1,16 @@
 extends Actor
 
+
+# the first function called on every node in the scene tree 
+# starting with the deepest node
+func _ready() -> void:
+	$AnimatedSprite.play()
+	_velocity.x = -speed.x # opposite direction to player
+
+func _physics_process(delta: float) -> void:
+	_velocity.y += gravity * delta
+	# change direction if hits wall
+	if is_on_wall():
+		_velocity.x *= -1.0
+		$AnimatedSprite.flip_h = !$AnimatedSprite.flip_h
+	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
