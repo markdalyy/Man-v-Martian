c31d95f25eb9caea8dc356d797a47fb6d4aeb8b8
Mark Daly <118356771@umail.ucc.ie>
2022-02-14 00:49:56 +0000

    Code player movement and animation

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,18 +1,25 @@
 extends Actor
 
-func _physics_process(delta: float) -> void:
+
+func _ready():
+	$AnimatedSprite.play()
+	
+	
+func _physics_process(_delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
-	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
-	# floor normal enables proper jump function
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL) # floor normal enables jump function
+	animate(direction)
+
 
 func get_direction() -> Vector2:
 	# input strength ranges from -1 (left/up) to 1 (right/down)
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
 		)
+		
 		
 func calculate_move_velocity(
 		linear_velocity: Vector2,
@@ -28,4 +35,28 @@ func calculate_move_velocity(
 	if is_jump_interrupted:
 		out.y = 0.0
 	return out
+	
+	
+func animate(direction: Vector2) -> void:
+	if direction.length() == 0:
+		$AnimatedSprite.animation = "stand" # idle default
+	if direction.x != 0:
+		$AnimatedSprite.animation = "run"
+		$AnimatedSprite.flip_h = direction.x < 0
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.offset = Vector2(0, 0) # reset offset
+	elif direction.y < 0:
+		#$AnimatedSprite.animation = "jump"
+		pass
+	elif Input.is_action_pressed("look_up"):
+		$AnimatedSprite.animation = "up"
+		if $AnimatedSprite.flip_h == false:
+			$AnimatedSprite.offset = Vector2(-4, -6)
+		else:
+			$AnimatedSprite.offset = Vector2(4, -6)
+	elif Input.is_action_pressed("lie_down"):
+		$AnimatedSprite.animation = "down"
+		$AnimatedSprite.offset = Vector2(0, 8)
+	else:
+		$AnimatedSprite.offset = Vector2(0, 0)
 		
